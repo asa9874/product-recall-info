b2856b6d708e3ea17b94c1ac5b38e0f68ed0ac8c
asa9874 <pjkpjk1129@gmail.com>
2025-01-24 20:28:00 +0900

    Feat: 카드 컴포넌트 및 메인 컨테이너 반응형

--- src/pages/Foods/components/Card.tsx
+++ src/pages/Foods/components/Card.tsx
@@ -2,8 +2,8 @@
 function Card() {
     return (
     <>
-        <div className="bg-zinc-50 w-64 h-60 p-4 ">
-            임시문장
+        <div className="bg-zinc-50 w-full h-80 p-4 rounded-lg shadow-md">
+            카드
         </div>
     </>
     )
--- src/pages/Foods/components/MainContainer.tsx
+++ src/pages/Foods/components/MainContainer.tsx
@@ -1,21 +1,21 @@
-import Card from "./Card"
+import Card from "./Card";
 
 function MainContainer() {
-    return (
+  return (
     <div className="place-items-center">
-        <div className="bg-neutral-200 grid grid-cols-3 gap-10 w-3/4 place-items-center shadow-xl rounded-md mt-10 p-3 h-screen">
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-        </div>
+      <div className="bg-neutral-200 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 w-3/4 place-items-center shadow-xl rounded-md mt-10 p-3 ">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
-    )
+  );
 }
 
-export default MainContainer
+export default MainContainer;
