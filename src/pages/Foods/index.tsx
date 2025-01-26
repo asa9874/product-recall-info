0ce48207d2ec866c421691dc2fc8a1a71dd6df35
asa9874 <pjkpjk1129@gmail.com>
2025-01-26 16:41:55 +0900

    Feat: 푸터 컴포넌트 추가

--- src/pages/Foods/index.tsx
+++ src/pages/Foods/index.tsx
@@ -1,11 +1,13 @@
 import Headers from "./components/Headers"
 import CardContainer from "./components/CardContainer"
+import Footer from "./components/Footer"
 
 function Foods() {
   return (
     <div className="bg-neutral-300">
       <Headers />
       <CardContainer />
+      <Footer/>
     </div>
   )
 }
