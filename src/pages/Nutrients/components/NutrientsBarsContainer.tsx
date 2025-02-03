64733261a2b4ab3d8b6199e96f80df1b7ae03f36
asa9874 <pjkpjk1129@gmail.com>
2025-02-04 02:17:37 +0900

    Feat: NutrientsBarsContainer에서 zustand를 사용하여 영양소 상태 관리 추가

--- src/pages/Nutrients/components/NutrientsBarsContainer.tsx
+++ src/pages/Nutrients/components/NutrientsBarsContainer.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MeterBar from './MeterBar';
+import { useNutrientStore } from '../context';
 
 interface Nutrient {
     name: string;
@@ -8,32 +9,37 @@ interface Nutrient {
 }
 
 function NutrientsBarsContainer() {
-    
-    const [nutrients, setNutrients] = useState<Nutrient[]>([
-        { name: '에너지 (kcal)', value: 0, max: 2600 },
-        { name: '단백질 (g)', value: 0, max: 65 },
-        { name: '지방 (g)', value: 0, max: 87 },
-        { name: '탄수화물 (g)', value: 0, max: 325 },
-        { name: '칼슘 (mg)', value: 0, max: 700 },
-        { name: '철 (mg)', value: 0, max: 10 },
-        { name: '칼륨 (mg)', value: 0, max: 3500 },
-        { name: '나트륨 (mg)', value: 0, max: 2000 },
-        { name: '비타민 A (μg RAE)', value: 0, max: 900 },
-        { name: '비타민 C (mg)', value: 0, max: 100 },
-        { name: '비타민 D (μg)', value: 0, max: 10 },
-        { name: '비타민 B1 (mg)', value: 0, max: 1.2 },
-        { name: '비타민 B2 (mg)', value: 0, max: 1.3 },
-        { name: '비타민 B6 (mg)', value: 0, max: 1.3 },
-        { name: '비타민 B12 (μg)', value: 0, max: 2.4 },
-        { name: '엽산 (DFE) (μg)', value: 0, max: 400 },
-        { name: '콜린 (mg)', value: 0, max: 550 },
-        { name: '판토텐산 (mg)', value: 0, max: 5 },
-        { name: '니아신 (mg)', value: 0, max: 16 },
-        { name: '포화지방산 (g)', value: 0, max: 20 },
-        { name: '콜레스테롤 (mg)', value: 0, max: 300 },
-        { name: '당류 (g)', value: 0, max: 50 },
-        { name: '식이섬유 (g)', value: 0, max: 25 },
-    ]);
+    const nutrientStore = useNutrientStore();
+
+    const [nutrients, setNutrients] = useState<Nutrient[]>([]);
+
+    useEffect(() => {
+        setNutrients([
+            { name: "에너지 (kcal)", value: nutrientStore.ENERGY_KCAL, max: 2600 },
+            { name: "단백질 (g)", value: nutrientStore.PROTEIN_G, max: 65 },
+            { name: "지방 (g)", value: nutrientStore.FAT_G, max: 87 },
+            { name: "탄수화물 (g)", value: nutrientStore.CARBOHYDRATE_G, max: 325 },
+            { name: "칼슘 (mg)", value: nutrientStore.CALCIUM_MG, max: 700 },
+            { name: "철 (mg)", value: nutrientStore.IRON_MG, max: 10 },
+            { name: "칼륨 (mg)", value: nutrientStore.POTASSIUM_MG, max: 3500 },
+            { name: "나트륨 (mg)", value: nutrientStore.SODIUM_MG, max: 2000 },
+            { name: "비타민 A (μg RAE)", value: nutrientStore.VITAMIN_A_UG_RAE, max: 900 },
+            { name: "비타민 C (mg)", value: nutrientStore.VITAMIN_C_MG, max: 100 },
+            { name: "비타민 D (μg)", value: nutrientStore.VITAMIN_D_UG, max: 10 },
+            { name: "비타민 B1 (mg)", value: nutrientStore.VITAMIN_B1_MG, max: 1.2 },
+            { name: "비타민 B2 (mg)", value: nutrientStore.VITAMIN_B2_MG, max: 1.3 },
+            { name: "비타민 B6 (mg)", value: nutrientStore.VITAMIN_B6_MG, max: 1.3 },
+            { name: "비타민 B12 (μg)", value: nutrientStore.VITAMIN_B12_UG, max: 2.4 },
+            { name: "엽산 (DFE) (μg)", value: nutrientStore.FOLATE_DFE_UG, max: 400 },
+            { name: "콜린 (mg)", value: nutrientStore.CHOLINE_MG, max: 550 },
+            { name: "판토텐산 (mg)", value: nutrientStore.PANTOTHENIC_ACID_MG, max: 5 },
+            { name: "니아신 (mg)", value: nutrientStore.NIACIN_MG, max: 16 },
+            { name: "포화지방산 (g)", value: nutrientStore.SATURATED_FAT_G, max: 20 },
+            { name: "콜레스테롤 (mg)", value: nutrientStore.CHOLESTEROL_MG, max: 300 },
+            { name: "당류 (g)", value: nutrientStore.SUGARS_G, max: 50 },
+            { name: "식이섬유 (g)", value: nutrientStore.DIETARY_FIBER_G, max: 25 },
+        ]);
+    }, [nutrientStore]);
 
     return (
         <div className="flex flex-col w-1/4 bg-white h-full rounded-xl p-5 shadow-xl dark:bg-gray-800 dark:text-white overflow-y-auto">
