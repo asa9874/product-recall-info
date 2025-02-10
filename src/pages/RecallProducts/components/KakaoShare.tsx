01c5d113f2906e02a528821469e8c77968ea84ff
asa9874 <pjkpjk1129@gmail.com>
2025-02-10 18:12:46 +0900

    Feat: 카카오 API 로직분리

--- src/pages/RecallProducts/components/KakaoShare.tsx
+++ src/pages/RecallProducts/components/KakaoShare.tsx
@@ -1,12 +1,5 @@
-/* eslint-disable */
-
 import { useEffect } from 'react';
-
-declare global {
-  interface Window {
-    Kakao: any;
-  }
-}
+import initializeKakaoAPI from '../apis/kakaoApi';
 
 interface KakaoShareProps {
   title: string;
@@ -21,54 +14,20 @@ function KakaoShareButton({
   imageUrl,
   link,
 }: KakaoShareProps) {
-  const APIKEY = import.meta.env.VITE_KAKAO_API_KEY;
-
   useEffect(() => {
     if (!window.Kakao) {
-      const script = document.createElement('script');
-      script.src = 'https://developers.kakao.com/sdk/js/kakao.js';
-      script.async = true;
-      script.onload = () => {
-        if (!window.Kakao.isInitialized()) {
-          window.Kakao.init(APIKEY);
-        }
-      };
-      document.body.appendChild(script);
-    } else if (!window.Kakao.isInitialized()) {
-      window.Kakao.init(APIKEY);
+      initializeKakaoAPI(title, description, imageUrl, link);
     }
-  }, []);
+  }, [title, description, imageUrl, link]);
 
   const shareKakao = () => {
-    if (window.Kakao) {
-      window.Kakao.Link.sendDefault({
-        objectType: 'feed',
-        content: {
-          title: title,
-          description: description,
-          imageUrl: imageUrl,
-          link: {
-            mobileWebUrl: link,
-            webUrl: link,
-          },
-        },
-        buttons: [
-          {
-            title: '더많은 회수제품 알아보기',
-            link: {
-              mobileWebUrl: link,
-              webUrl: link,
-            },
-          },
-        ],
-      });
-    }
+    initializeKakaoAPI(title, description, imageUrl, link);
   };
 
   return (
     <button
       onClick={shareKakao}
-      className="bg-yellow-400 text-black p-1 rounded-lg hover:bg-yellow-500 transition flex items-center absolute right-4 top-0"
+      className="bg-yellow-400 text-black p-1 rounded-lg hover:bg-yellow-500 transition flex items-center"
     >
       <img src="/kakao.png" alt="카카오톡 로고" className="w-6 h-6" />
     </button>
