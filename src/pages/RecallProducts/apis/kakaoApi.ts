552479d64332dde2baa4ce72d36fc04ec6fa8d46
asa9874 <pjkpjk1129@gmail.com>
2025-02-10 19:17:10 +0900

    Refactor: 카카오 API 호출 방식을 axios에서 Kakao SDK로 변경 및 초기화 로직 개선

--- src/pages/RecallProducts/apis/kakaoApi.ts
+++ src/pages/RecallProducts/apis/kakaoApi.ts
@@ -5,32 +5,70 @@ declare global {
   }
 }
 
-import axios from 'axios';
-
-const initializeKakaoAPI = async (
+const initializeKakaoAPI = (
   title: string,
   description: string,
   imageUrl: string,
   link: string
 ) => {
-  try {
-    const response = await axios.post(
-      '/.netlify/functions/kakaoApi',
-      {
-        title,
-        description,
-        imageUrl,
-        link,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
+  const APIKEY = import.meta.env.VITE_KAKAO_API_KEY;
+  if (!window.Kakao) {
+    const script = document.createElement('script');
+    script.src = 'https://developers.kakao.com/sdk/js/kakao.js';
+    script.async = true;
+    script.onload = () => {
+      if (!window.Kakao.isInitialized()) {
+        window.Kakao.init(APIKEY);
       }
-    );
-    console.log(response.data);
-  } catch (error) {
-    console.error('카카오톡 공유 오류', error);
+      // 카카오톡 메시지 보내기
+      window.Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+          title: title,
+          description: description,
+          imageUrl: imageUrl,
+          link: {
+            mobileWebUrl: link,
+            webUrl: link,
+          },
+        },
+        buttons: [
+          {
+            title: '더많은 회수제품 알아보기',
+            link: {
+              mobileWebUrl: link,
+              webUrl: link,
+            },
+          },
+        ],
+      });
+    };
+    document.body.appendChild(script);
+  } else if (!window.Kakao.isInitialized()) {
+    window.Kakao.init(APIKEY); // 이미 SDK가 로드되었으면 초기화
+  } else {
+    // 카카오 SDK가 이미 초기화된 경우, 바로 카카오톡 메시지 보내기
+    window.Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+        link: {
+          mobileWebUrl: link,
+          webUrl: link,
+        },
+      },
+      buttons: [
+        {
+          title: '더많은 회수제품 알아보기',
+          link: {
+            mobileWebUrl: link,
+            webUrl: link,
+          },
+        },
+      ],
+    });
   }
 };
 
