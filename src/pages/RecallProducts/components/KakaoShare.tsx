a9e846a95466974dfb84a92cf47fafcefcf534bf
asa9874 <pjkpjk1129@gmail.com>
2025-02-10 19:15:16 +0900

    Refactor: 카카오 API 호출 방식을 axios에서 Kakao SDK로 변경 및 관련 코드 정리

--- src/pages/RecallProducts/components/KakaoShare.tsx
+++ src/pages/RecallProducts/components/KakaoShare.tsx
@@ -1,30 +1,75 @@
-import initializeKakaoAPI from '../apis/kakaoApi';
-
-interface KakaoShareProps {
-  title: string;
-  description: string;
-  imageUrl: string;
-  link: string;
+/* eslint-disable */
+declare global {
+  interface Window {
+    Kakao: any;
+  }
 }
 
-function KakaoShareButton({
-  title,
-  description,
-  imageUrl,
-  link,
-}: KakaoShareProps) {
-  const shareKakao = () => {
-    initializeKakaoAPI(title, description, imageUrl, link);
-  };
+const initializeKakaoAPI = (
+  title: string,
+  description: string,
+  imageUrl: string,
+  link: string
+) => {
+  const APIKEY = import.meta.env.VITE_KAKAO_API_KEY;
+  if (!window.Kakao) {
+    const script = document.createElement('script');
+    script.src = 'https://developers.kakao.com/sdk/js/kakao.js';
+    script.async = true;
+    script.onload = () => {
+      if (!window.Kakao.isInitialized()) {
+        window.Kakao.init(APIKEY);
+      }
+      // 카카오톡 메시지 보내기
+      window.Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+          title: title,
+          description: description,
+          imageUrl: imageUrl,
+          link: {
+            mobileWebUrl: link,
+            webUrl: link,
+          },
+        },
+        buttons: [
+          {
+            title: '더많은 회수제품 알아보기',
+            link: {
+              mobileWebUrl: link,
+              webUrl: link,
+            },
+          },
+        ],
+      });
+    };
+    document.body.appendChild(script);
+  } else if (!window.Kakao.isInitialized()) {
+    window.Kakao.init(APIKEY); // 이미 SDK가 로드되었으면 초기화
+  } else {
+    // 카카오 SDK가 이미 초기화된 경우, 바로 카카오톡 메시지 보내기
+    window.Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+        link: {
+          mobileWebUrl: link,
+          webUrl: link,
+        },
+      },
+      buttons: [
+        {
+          title: '더많은 회수제품 알아보기',
+          link: {
+            mobileWebUrl: link,
+            webUrl: link,
+          },
+        },
+      ],
+    });
+  }
+};
 
-  return (
-    <button
-      onClick={shareKakao}
-      className="bg-yellow-400 text-black p-2 rounded-lg hover:bg-yellow-500 transition flex items-center absolute right-4 top-0"
-    >
-      <img src="/kakao.png" alt="카카오톡 로고" className="w-5 h-5" />
-    </button>
-  );
-}
-
-export default KakaoShareButton;
+export default initializeKakaoAPI;
